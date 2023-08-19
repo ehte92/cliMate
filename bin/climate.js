809012d85a84a97951a9b36b5873938eb8e8f227
Ehtesham <ehte92@gmail.com>
2023-08-20 00:10:29 +0400

    bug fixes

--- bin/climate.js
+++ bin/climate.js
@@ -5,7 +5,7 @@
 /* eslint-disable no-restricted-syntax */
 const ora = require("ora");
 const chalk = require("chalk");
-const CliTable3 = require("cli-table3");
+const Table = require("cli-table3");
 const { prompt } = require("enquirer");
 const getWeather = require("../lib/getWeather");
 const Icons = require("../icons/icons.json");
@@ -13,111 +13,114 @@ const getForecast = require("../lib/getForecast");
 
 const spinner = ora();
 
-const weather = (city) => new Promise((resolve, reject) => {
-    spinner.start("beaming the weather data from space...🛰");
-    getWeather(city)
-        .then((res) => {
-            const weatherIcon = Icons[res.icon];
+const weather = (city) =>
+    new Promise((resolve, reject) => {
+        spinner.start("beaming the weather data from space...🛰");
+        getWeather(city)
+            .then((res) => {
+                const weatherIcon = Icons[res.icon];
 
-            const weatherData = Object.keys(res.main)
-                .map(
-                    (key) => `${chalk.whiteBright.bold(key)} : ${chalk.white(res.main[key] || chalk.gray("n\\a"))}`,
-                )
-                .filter(Boolean)
-                .join("\n");
+                const weatherData = Object.keys(res.main)
+                    .map(
+                        (key) => `${chalk.whiteBright.bold(key)} : ${chalk.white(res.main[key] || chalk.gray("n\\a"))}`,
+                    )
+                    .filter(Boolean)
+                    .join("\n");
 
-            const weatherTable = new CliTable3({
-                chars: {
-                    top: "",
-                    "top-mid": "",
-                    "top-left": "",
-                    "top-right": "",
-                    bottom: "",
-                    "bottom-mid": "",
-                    "bottom-left": "",
-                    "bottom-right": "",
-                    left: "",
-                    "left-mid": "",
-                    mid: "",
-                    "mid-mid": "",
-                    right: "",
-                    "right-mid": "",
-                    middle: " ",
-                },
-                style: {
-                    "padding-left": 4,
-                    head: ["yellow", "bold"],
-                },
-            });
-            weatherTable.push([`\n${weatherData}`, weatherIcon]);
-            spinner.stop();
-            resolve(weatherTable.toString());
-        })
-        .catch((error) => {
-            if (error?.message) {
-                spinner.fail(error?.message);
-                return;
-            }
-            spinner.fail("Sattelite is not responding, please try again later.");
-            reject(error);
-        });
-});
-
-const foreCast = (city) => new Promise((resolve, reject) => {
-    spinner.start("forecasting the future...🔮");
-    getForecast(city)
-        .then((res) => {
-            const table = new CliTable3({
-                head: [
-                    { colSpan: 2, content: "Time" },
-                    "Temperature",
-                    "Weather",
-                    "Humidity",
-                    "Visibility",
-                    "Wind",
-                    "Cloud",
-                    "Rain",
-                    "snow",
-                ],
-                style: {
-                    border: ["gray"],
-                    head: ["brightRed", "bold"],
-                },
-                colAligns: ["center"],
-            });
-
-            for (const day of Object.keys(res)) {
-                for (const [i, dayObj] of res[day].entries()) {
-                    const tableData = [];
-                    if (i === 0) {
-                        tableData.push({
-                            rowSpan: 4,
-                            content: chalk.green.bold(day),
-                            vAlign: "center",
-                        });
-                    }
-                    tableData.push(...dayObj);
-                    table.push(tableData);
+                const weatherTable = new Table({
+                    chars: {
+                        top: "",
+                        "top-mid": "",
+                        "top-left": "",
+                        "top-right": "",
+                        bottom: "",
+                        "bottom-mid": "",
+                        "bottom-left": "",
+                        "bottom-right": "",
+                        left: "",
+                        "left-mid": "",
+                        mid: "",
+                        "mid-mid": "",
+                        right: "",
+                        "right-mid": "",
+                        middle: " ",
+                    },
+                    style: {
+                        "padding-left": 4,
+                        head: ["yellow", "bold"],
+                    },
+                });
+                weatherTable.push([`\n${weatherData}`, weatherIcon]);
+                spinner.stop();
+                resolve(weatherTable.toString());
+            })
+            .catch((error) => {
+                if (error?.message) {
+                    spinner.fail(error?.message);
+                    return;
                 }
-            }
+                spinner.fail("Sattelite is not responding, please try again later.");
+                reject(error);
+            });
+    });
 
-            spinner.stop();
-            resolve(chalk.yellow.bold("\n\n Weather Forecast 🔮:\n") + table.toString());
-        })
-        .catch((error) => {
-            if (error?.message) {
-                spinner.fail(error?.message);
+const foreCast = (city) =>
+    new Promise((resolve, reject) => {
+        spinner.start("forecasting the future...🔮");
+        getForecast(city)
+            .then((res) => {
+                const table = new Table({
+                    head: [
+                        { colSpan: 2, content: "Time" },
+                        "Temperature",
+                        "Weather",
+                        "Humidity",
+                        "Visibility",
+                        "Wind",
+                        "Cloud",
+                        "Rain",
+                        "snow",
+                    ],
+                    style: {
+                        border: ["gray"],
+                        head: ["brightRed", "bold"],
+                    },
+                    colAligns: ["center"],
+                });
+
+                for (const day of Object.keys(res)) {
+                    for (const [i, dayObj] of res[day].entries()) {
+                        const tableData = [];
+                        if (i === 0) {
+                            tableData.push({
+                                rowSpan: 4,
+                                content: chalk.green.bold(day),
+                                vAlign: "center",
+                            });
+                        }
+                        tableData.push(...dayObj);
+                        table.push(tableData);
+                    }
+                }
+
+                spinner.stop();
+                resolve(chalk.yellow.bold("\n\n Weather Forecast 🔮:\n") + table.toString());
+            })
+            .catch((error) => {
+                if (error?.message) {
+                    spinner.fail(error?.message);
+                    reject();
+                    return;
+                }
+                spinner.fail("Sattelite is not responding, please try again later.");
                 reject();
-                return;
-            }
-            spinner.fail("Sattelite is not responding, please try again later.");
-            reject();
-        });
-});
+            });
+    });
 
-async () => {
-    const city = process.argv[2]
-        || (
+(async () => {
+    const city =
+        process.argv[2] ||
+        (
             await prompt({
                 type: "input",
                 name: "city",
@@ -132,4 +135,4 @@ async () => {
 
     const forecastData = await foreCast(city);
     console.log(forecastData);
-};
+})();
